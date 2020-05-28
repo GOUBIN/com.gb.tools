d7362cb07043cfd373f6d0aeaa0ba89326f96122
GOUBIN <1414150215@QQ.COM>
2020-05-28 18:05:17 +0800

    Initial commit

---
+++ Runtime/MyTools.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GBGames
+{
+    public class MyTools : MonoBehaviour
+    {
+        // Start is called before the first frame update
+
+        public static string age = "213213";
+    }
+
+}
